8f1ea03ea523697a414bdbe3b5880d83ae543e92
Blezin <blezin2205@gmail.com>
2020-12-17 22:34:35 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_bkgrd_tap/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_bkgrd_tap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyHomePage(),
+    ),
+  );
+}
+
+
+class MyHomePage extends StatefulWidget {
+
+
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  double _randomOpacity = 1.0;
+  Random random = new Random();
+
+
+  Color get randomColor {
+    return Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(_randomOpacity);
+}
+
+  void _incrementCounter() {
+    setState(() => _randomOpacity = random.nextDouble() );
+    print(_randomOpacity);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: () => _incrementCounter(),
+      child: Scaffold(
+        backgroundColor: randomColor,
+        appBar: AppBar(
+          title: Text("Random Background App"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Hex color for random background:',
+                style: TextStyle(
+                    color: Colors.white, fontSize: 18),
+              ),
+              Text(
+                randomColor.toString().replaceAll("Color", "",)
+                    .replaceAll("(", "")
+                  .replaceAll(")", ""),
+                style: TextStyle(
+                  color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold
+                ),
+              ),
+            ],
+          ),
+        ),
+
+      ),
+    );
+  }
+}
